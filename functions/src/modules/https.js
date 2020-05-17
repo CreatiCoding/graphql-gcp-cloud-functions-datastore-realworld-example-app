a216599685e9eb7faea8f04d548948fde90ed280
CreatiCoding <creaticoding@gmail.com>
2020-05-17 17:10:35 +0900

    add endpoint of cloud function

--- functions/src/modules/https.js
+++ functions/src/modules/https.js
@@ -8,6 +8,7 @@ module.exports = function ({ functions, exports }) {
       });
 
       // http://localhost:5001/graphql-fd-realword/us-central1/api/graphql?query={posts{title%20post_id}}
+      // https://us-central1-graphql-fd-realword.cloudfunctions.net/api/graphql?query=%7Bposts%7Btitle%20post_id%7D%7D
       controller.api = functions.https.onRequest(app);
 
       console.log("https load completed");
